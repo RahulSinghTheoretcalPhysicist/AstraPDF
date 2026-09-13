60d60ab9061e4778c2415288d0d391b25602d8dc
RahulSinghTheoretcalPhysicist <rahul.singh@msmail.ariel.ac.il>
2026-09-14 00:03:22 +0300

    Add reliable search and dock controls

---
+++ src/UiEnhancer.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <QObject>
+
+class MainWindow;
+class QDockWidget;
+class QLineEdit;
+class QComboBox;
+class QTextBrowser;
+class QPushButton;
+class QNetworkAccessManager;
+
+class UiEnhancer final : public QObject {
+    Q_OBJECT
+public:
+    explicit UiEnhancer(MainWindow *window, QObject *parent=nullptr);
+
+private:
+    void decorateDock(QDockWidget *dock);
+    void setupSearch();
+    void runSearch();
+    void runDuckDuckGo(const QString& query);
+    void runWikipedia(const QString& query);
+
+    MainWindow *m_window=nullptr;
+    QNetworkAccessManager *m_network=nullptr;
+    QDockWidget *m_webDock=nullptr;
+    QLineEdit *m_webInput=nullptr;
+    QComboBox *m_provider=nullptr;
+    QTextBrowser *m_webResult=nullptr;
+    QPushButton *m_webButton=nullptr;
+    QString m_activeQuery;
+};

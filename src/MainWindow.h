be75d8be9a64b496c2737b649497b0340d728568
RahulSinghTheoretcalPhysicist <rahul.singh@msmail.ariel.ac.il>
2026-09-13 22:53:05 +0300

    Add interactive neon theme and dictionary UI state

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <QMainWindow>
-class QAction; class QComboBox; class QLabel; class QLineEdit; class QPdfDocument;
-class QResizeEvent; class QScrollArea; class QSpinBox; class PdfCanvas;
+class QAction; class QComboBox; class QDockWidget; class QLabel; class QLineEdit; class QPdfDocument;
+class QResizeEvent; class QScrollArea; class QSpinBox; class QTextBrowser; class QTimer; class PdfCanvas;
 
 class MainWindow final : public QMainWindow {
     Q_OBJECT
@@ -12,16 +12,28 @@ protected:
     void resizeEvent(QResizeEvent *event) override;
 private:
     void buildUi(); void buildActions(); void buildToolbar();
+    void buildDictionaryDock();
     void applyModeFromCombo(); void updatePageUi(int page); void updateZoomUi(); void inspectCos();
     void finishOpenPdf();
+    void applyTheme();
+    void setNeonEnabled(bool enabled);
+    void advanceNeon();
+    void lookupDictionaryWord(const QString& rawWord);
 
     QPdfDocument *m_document=nullptr;
     QScrollArea *m_scrollArea=nullptr;
     PdfCanvas *m_canvas=nullptr;
     QAction *m_open=nullptr,*m_copy=nullptr,*m_highlight=nullptr,*m_cosInspect=nullptr;
+    QAction *m_neonAction=nullptr,*m_dictionaryAction=nullptr;
     QSpinBox *m_pageSpin=nullptr;
     QLabel *m_pageTotal=nullptr,*m_zoomLabel=nullptr;
     QComboBox *m_modeCombo=nullptr;
     QLineEdit *m_search=nullptr;
+    QDockWidget *m_dictionaryDock=nullptr;
+    QLineEdit *m_dictionaryInput=nullptr;
+    QTextBrowser *m_dictionaryResult=nullptr;
+    QTimer *m_neonTimer=nullptr;
+    bool m_neonEnabled=true;
+    int m_neonHue=190;
     QString m_currentFile;
 };

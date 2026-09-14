fe60b4043ed6bee853a4b0fe8114da94683e5124
RahulSinghTheoretcalPhysicist <rahul.singh@msmail.ariel.ac.il>
2026-09-14 19:54:27 +0300

    Make targeted rail cleanup static

--- src/TargetedRailFixController.h
+++ src/TargetedRailFixController.h
@@ -3,20 +3,15 @@
 
 class MainWindow;
 class QToolBar;
-class QEvent;
 
 class TargetedRailFixController final : public QObject {
     Q_OBJECT
 public:
     explicit TargetedRailFixController(MainWindow *window, QObject *parent=nullptr);
 
-protected:
-    bool eventFilter(QObject *watched, QEvent *event) override;
-
 private:
     void applyFixes();
 
     MainWindow *m_window=nullptr;
     QToolBar *m_toolbar=nullptr;
-    bool m_applying=false;
 };

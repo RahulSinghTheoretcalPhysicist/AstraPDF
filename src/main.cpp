fce155e3b3b79448f3ded38e94823797759421e9
RahulSinghTheoretcalPhysicist <rahul.singh@msmail.ariel.ac.il>
2026-09-14 19:22:18 +0300

    Remove targeted rail fixer to stop UI blinking

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 #include "BackgroundController.h"
 #include "InterfaceController.h"
 #include "RailPolishController.h"
-#include "TargetedRailFixController.h"
 
 int main(int argc, char *argv[])
 {
@@ -21,7 +20,6 @@ int main(int argc, char *argv[])
     BackgroundController backgrounds(&w,&w);
     InterfaceController interfaceController(&w,&w);
     RailPolishController railPolish(&w,&w);
-    TargetedRailFixController targetedRailFix(&w,&w);
     w.show();
 
     if(argc>1)

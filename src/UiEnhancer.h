75a2c9db23451d4ed6c04e827e9ea50184e4e0af
RahulSinghTheoretcalPhysicist <rahul.singh@msmail.ariel.ac.il>
2026-09-14 16:30:48 +0300

    Restore UiEnhancer interface

--- src/UiEnhancer.h
+++ src/UiEnhancer.h
@@ -13,26 +13,16 @@ class QComboBox;
 class QTextBrowser;
 class QPushButton;
 class QNetworkAccessManager;
-class QWidget;
-class QTimer;
-class QMenu;
 
 class UiEnhancer final : public QObject {
     Q_OBJECT
 public:
     explicit UiEnhancer(MainWindow *window, QObject *parent=nullptr);
 
-protected:
-    bool eventFilter(QObject *watched, QEvent *event) override;
-
 private:
     void decorateDock(QDockWidget *dock);
     void setupSearch();
     void setupNeonBackground();
-    void setupEdgeRail();
-    void positionEdgeRail();
-    void showEdgeRail();
-    void scheduleEdgeRailHide();
     void applyNeonBackground(const QString& colorName, bool persist=true);
     void runSearch();
     void runDuckDuckGo(const QString& query);
@@ -46,11 +36,5 @@ private:
     QComboBox *m_provider=nullptr;
     QTextBrowser *m_webResult=nullptr;
     QPushButton *m_webButton=nullptr;
-    QWidget *m_edgeRail=nullptr;
-    QWidget *m_pdfSearchPopup=nullptr;
-    QWidget *m_windowControls=nullptr;
-    QTimer *m_edgeHideTimer=nullptr;
-    QMenu *m_modeMenu=nullptr;
-    QComboBox *m_modeCombo=nullptr;
     QString m_activeQuery;
 };
